11093f4101d7bbf7dc2c2b959ccffb945ecb3ec5
Weiheng <tweiheng@umich.edu>
2022-04-10 00:52:40 -0400

    Add state validity checking

--- include/state_validity.h
+++ include/state_validity.h
@@ -5,13 +5,16 @@
 
 class StateValidityChecker {
 public:
-    StateValidityChecker(); // TODO(tweiheng): init with pointers to planning scene & joint model group
+    StateValidityChecker(planning_scene::PlanningScene planning_scene); // TODO(tweiheng): init with pointers to planning scene & joint model group
 
     // Checks if a configuration is valid
     bool CheckValidity(const Eigen::VectorXd& q);
 
 private:
     // TODO(tweiheng): add pointers to planning scene & joint model group
+    planning_scene::PlanningScene planning_scene;
+    collision_detection::CollisionRequest collision_request;
+    collision_detection::CollisionResult collision_result;
 };
 
 #endif // STATE_VALIDITY_H
--- src/state_validity.cpp
+++ src/state_validity.cpp
@@ -1,11 +1,20 @@
 #include "state_validity.h"
 #include <Eigen/Dense>
 
+#include <moveit/move_group_interface/move_group_interface.h>
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
+
 // TODO(tweiheng): init with pointers to planning scene & joint model group
-StateValidityChecker::StateValidityChecker() {}
+StateValidityChecker::StateValidityChecker(planning_scene::PlanningScene ps) {
+    planning_scene = ps;
+}
 
 // Checks if a configuration is valid
 bool StateValidityChecker::CheckValidity(const Eigen::VectorXd& q) {
     // TODO(tweiheng): check validity (collisions + joint limits) using MoveIt
-    return true;
+    moveit::core::RobotState& current_state = planning_scene.getCurrentStateNonConst();
+    current_state.setJointGroupPositions(joint_model_group, q);
+
+    planning_scene.checkCollision(collision_request, collision_result, current_state);
+    return collision_result.collision;
 }
